6a1a551ced4aece0109bf2c52da5bc5040173a8f
AlexKarpinets <66379289+AlexKarpinets@users.noreply.github.com>
2021-09-07 20:03:59 +0300

    Initial Commit

---
+++ TrafficLights SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrafficLights SwiftUI
+//
+//  Created by Karpinets Alexander on 07.09.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TrafficLights SwiftUI/TrafficLights_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLights_SwiftUIApp.swift
+//  TrafficLights SwiftUI
+//
+//  Created by Karpinets Alexander on 07.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLights_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
